17410276f0de88be21f940a4e43c3139a4ec063c
orange <309871271@qq.com>
2023-08-11 09:38:16 +0800

    支持多环境变量

--- jd_tg_monitor.py
+++ jd_tg_monitor.py
@@ -1,13 +1,13 @@
 import asyncio
 import datetime
-import functools
 import re
+import signal
+import sys
 from collections import defaultdict
-from concurrent.futures._base import CancelledError
 
 import aiohttp
 import yaml
-from telethon import errors, events
+from telethon import events
 
 from qinglong import init_ql
 from utils import get_logger, get_tg_client
@@ -77,70 +77,75 @@ async def parse_env(env_str):
 
 
 async def parse_message(raw_text):
+    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S %f")
     raw_text_lines = raw_text.splitlines()
 
-    act_name = None
-    if len(raw_text_lines) == 2:
+    try:
         act_name = raw_text_lines[0]
-        env_str = raw_text_lines[1]
+        pattern = re.compile(r"[^\u4e00-\u9fa5]")
+        act_name = act_name.split("·")[0]
+        act_name = re.sub(pattern, "", act_name)
+    except Exception as e:
+        act_name = None
 
-        try:
+    env_list = []
+
+    try:
+        env_str_list = raw_text_lines[1:]
+        for env_str in env_str_list:
             env_name, value = await parse_env(env_str)
+            if not env_name:
+                continue
 
-            pattern = re.compile(r"[^\u4e00-\u9fa5]")
-            act_name = act_name.split("·")[0]
-            act_name = re.sub(pattern, "", act_name)
-        except Exception:
-            return None, None
-    else:
-        for env_str in raw_text_lines:
-            env_name, value = await parse_env(env_str)
-            if env_name:
-                break
+            env_list.append(
+                {
+                    "name": env_name,
+                    "value": value,
+                    "remarks": f"[AutoGen]-[{now}]",
+                }
+            )
+    except Exception as e:
+        pass
 
-    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S %f")
-    env = {
-        "name": env_name,
-        "value": value,
-        "remarks": f"[AutoGen]-[{now}]",
-    }
-    logger.info(f"获取到环境变量：{env}")
-    return env, act_name
+    logger.info(f"获取到环境变量：{env_list}")
+    return env_list, act_name
 
 
 async def handler(event):
     raw_text = event.raw_text
     logger.info(f"检测到消息 \n{raw_text}")
 
-    env, act_name = await parse_message(raw_text)
-    if not env:
+    env_list, act_name = await parse_message(raw_text)
+    if not env_list:
         return
 
-    env_name = env["name"]
-
-    logger.info(f"开始检测环境变量：【{env_name}】是否存在")
-    exist_env = await env_exist(env_name)
-    if not exist_env:
-        logger.info(f"环境变量【{env_name}】不存在，插入！")
-        ret = ql.insert_env([env])
-        env_id = ret[0]["id"]
-    else:
-        logger.info(f"环境变量【{env_name}】存在，更新！")
-        env_id = exist_env["id"]
-        env["id"] = env_id
-        ret = ql.put_env(env)
-
     tasks = []
-    if env_name in config_map:
-        logger.warning(f"环境变量【{env_name}】在配置文件中")
+    for env in env_list:
+        env_name = env["name"]
+        value = env["value"]
 
-        task_info = config_map[env_name]
-        script = task_info["Script"]
-        if script not in task_id_map:
-            logger.warning(f"环境变量【{env_name}】task配置中, 跳过")
-            return
+        exist_env = await env_exist(env_name)
+        if not exist_env:
+            logger.info(f"环境变量【{env_name}】不存在，插入！")
+            ret = ql.insert_env([env])
+            env_id = ret[0]["id"]
+        else:
+            if value != exist_env["value"]:
+                logger.info(f"环境变量【{env_name}】存在，且不一致，更新！")
+                env_id = exist_env["id"]
+                env["id"] = env_id
+                ret = ql.put_env(env)
 
-        tasks = task_id_map[script]
+        if env_name in config_map:
+            logger.warning(f"环境变量【{env_name}】在配置文件中")
+
+            task_info = config_map[env_name]
+            script = task_info["Script"]
+            if script not in task_id_map:
+                logger.warning(f"环境变量【{env_name}】task配置中, 跳过")
+                continue
+
+            tasks.append(task_id_map[script])
 
     if act_name:
         for k, v in task_name_map.items():
@@ -179,18 +184,7 @@ async def main():
             handler, events.NewMessage(pattern=r".*\n*export \w+=\"[^\"]+\"")
         )
 
-        while client.is_connected():
-            try:
-                await client.run_until_disconnected()
-            except Exception:
-                pass
-        else:
-            try:
-                await client.connect()
-            except OSError:
-                print("Failed to connect")
-            await asyncio.sleep(1)
-        # client.loop.run_forever()
+        await client.run_until_disconnected()
 
 
 if __name__ == "__main__":
