55e096a8a147b8133271e4935d0f4cfef11bc5e1
orange <309871271@qq.com>
2025-01-13 17:51:27 +0800

    save

--- jd_cookie_copy.py
+++ jd_cookie_copy.py
@@ -21,35 +21,37 @@ async def main():
     ql = init_ql()
     cookies = await get_cookies(ql)
 
-    host = '127.0.0.1:5700'
+    host = 'http://127.0.0.1:5700'
     client_id = '0eRdY1f-NmOH'
     client_secret = 'akP6_WpzMI35aOX6Njg-uqqJ'
-
 
     json_config = {"host": host, "client_id": client_id, "client_secret": client_secret}
 
     local_qinglong = Qinglong(json_config)
     local_cookies = await get_cookies(local_qinglong)
-    local_cookies.delete_env([x['id'] for x in local_cookies])
+    if local_cookies:
+        local_qinglong.delete_env([x['id'] for x in local_cookies])
 
     for ck in cookies:
         ck_env_dict = [{
-            "value": ck,
+            "value": ck['value'],
             "name": "JD_COOKIE",
-            "remarks": f"自动新增",
+            "remarks": f"{ck['remarks']}自动新增",
         }]
-        local_cookies.insert_env(data=ck_env_dict)
+        local_qinglong.insert_env(data=ck_env_dict)
 
 
     msg = "\n".join(
         [
-            f"{cookie['remarks'].split('@')[0]} cookie 作废成功"
+            f"{cookie['remarks'].split('@')[0]} cookie 新增成功"
             for cookie in cookies
         ]
     )
-    send("京东cookie强制失效", msg)
+    send("京东cookie-Copy成功", msg)
 
 
 
 if __name__ == "__main__":
     asyncio.run(main())
+    import os
+    os.system('curl 127.0.0.1:5700')
