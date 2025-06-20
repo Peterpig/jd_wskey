bb78b86bbd83eb8cf005a8ac13048b6022a94a73
zhoupeilin <309871271@qq.com>
2025-06-20 16:13:48 +0800

    fox

--- jd_cookie_kill.py
+++ jd_cookie_kill.py
@@ -2,6 +2,8 @@
 new Env('京东cookie强制作废');
 cron: 1 1 * * * jd_cookie_kill.py
 """
+import random
+import requests
 import asyncio
 import logging
 
@@ -16,11 +18,48 @@ except:
     send = lambda *args: ...
 
 
+async def need_login(
+        cookie: str
+):
+    """
+    检测JD_COOKIE是否失效
+
+    :param cookie: 就是cookie
+    """
+    url = "https://me-api.jd.com/user_new/info/GetJDUserInfoUnion"
+    method = 'GET'
+    headers = {
+        "Host": "me-api.jd.com",
+        "Accept": "*/*",
+        "Connection": "keep-alive",
+        "Cookie": cookie,
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36 Edg/106.0.1370.42",
+        "Accept-Language": "zh-cn",
+        "Referer": "https://home.m.jd.com/myJd/newhome.action?sceneval=2&ufc=&",
+        "Accept-Encoding": "gzip, deflate, br"
+    }
+    r =  requests.request(method, url, headers=headers)
+    # 检测这里太快了, sleep一会儿, 避免FK
+    await asyncio.sleep(random.uniform(0.5,2))
+
+    try:
+        if r.json().get('retcode') == str(1001):
+            return True
+    except:
+        return True
+
+    return False
+
 async def main():
     ql = init_ql()
     cookies = await get_cookies(ql)
 
-    disable_cookies_ids = [x['id'] for x in cookies]
+    disable_cookies_ids = []
+
+    for cookie in cookies:
+        if await need_login(cookie['value']):
+            disable_cookies_ids.append(cookie['id'])
+
     ql.disable_env(disable_cookies_ids)
 
 
